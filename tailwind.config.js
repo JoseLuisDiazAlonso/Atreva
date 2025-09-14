69aa461243be2e269441428f8458cb0400132e63
Jose Luis Díaz Alonso <134778793+JoseLuisDiazDev@users.noreply.github.com>
2025-09-14 22:53:59 +0200

    Commit de tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export const content = [
+  "./index.html",
+  "./src/**/*.{js,jsx}",
+];
+
+export const theme = {
+  extend: {
+    fontFamily: {
+      sans: ['Poppins', 'sans-serif'],
+    },
+  },
+};
